7d7b53c5a4d219a7a36d3a99cca94432e288cf97
Unknown <sshivarov@yahoo.com>
2019-03-27 02:54:23 +0200

    Initial commit
    Commit the Visual Studio solution files

---
+++ TextToSpeech_BG_Voice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextToSpeech_BG_Voice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
